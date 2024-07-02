c9c22ccecedef198620efe03d6b086175296741d
Luka Gobejishvili <119331133+Gobeja666@users.noreply.github.com>
2024-07-02 15:29:38 +0400

    0.3

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
-const HeaderEng = [`About`, `Contacts`, `Log In / Sign Up`, `Gallery`, `Services`]
-const HeaderGeo = [`ინფორმაცია`, `კონტაქტები`, `შესვლა / დარეგისტრირება`, `გალერეა`, `მომსახურება`]
+const HeaderEng = [`About`, `Contacts`, `Log In`, `Gallery`, `Services`]
+const HeaderGeo = [`ინფორმაცია`, `კონტაქტები`, `შესვლა`, `გალერეა`, `მომსახურება`]
 
 const about = document.getElementById(`about`)
 const contact = document.getElementById(`contact`)
@@ -16,10 +16,22 @@ function Headereng(){
 }
 Headereng()
 
-function Headergeo(){
-    about.textContent = `${HeaderGeo[0]}`
-    contact.textContent = `${HeaderGeo[1]}`
-    login_register.textContent = `${HeaderGeo[2]}`
-    gallery.textContent = `${HeaderGeo[3]}`
-    services.textContent = `${HeaderGeo[4]}`
+function ChangeLang(){
+    const selectElement = document.getElementById(`language`)
+    const selectedValue = selectElement.value;
+
+    if(selectedValue === `En`){
+        about.textContent = `${HeaderEng[0]}`
+        contact.textContent = `${HeaderEng[1]}`
+        login_register.textContent = `${HeaderEng[2]}`
+        gallery.textContent = `${HeaderEng[3]}`
+        services.textContent = `${HeaderEng[4]}`
+    }else if(selectedValue === `Geo`){
+        about.textContent = `${HeaderGeo[0]}`
+        contact.textContent = `${HeaderGeo[1]}`
+        login_register.textContent = `${HeaderGeo[2]}`
+        gallery.textContent = `${HeaderGeo[3]}`
+        services.textContent = `${HeaderGeo[4]}`
+    }
+
 }
